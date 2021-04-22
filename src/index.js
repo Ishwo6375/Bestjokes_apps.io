408e778cf311fc3d0270b9ceb91cf8cd0ba48aad
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-04-21 22:32:20 -0400

    added like dislike button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+  const createLikeButton = () =>{
+  const newDiv = document.createElement('div');
+  newDiv.id = "respond"
+  const header = document.createElement('h3')
+  const likeBtn = document.createElement('button');
+  
+  const likeBtn2 = document.createElement('button');
+
+  header.innerText = "Do you like this?"
+  header.id = 'heading1'
+  likeBtn.innerHTML = "Like"
+  likeBtn2.innerHTML = "Dislike"
+
+//   body.newDiv.appendChild(header);
+//   body.newDiv.appendChild(likeBtn);
+//   newDiv.appendChild(likeBtn2);
+//   return newDiv;
+  document.body.appendChild(newDiv);
+   newDiv.appendChild(header);
+   newDiv.appendChild(likeBtn);
+   newDiv.appendChild(likeBtn2);
+
+   return newDiv;
+
+  }
+
+
+   createLikeButton();
 //  <div id="joke" class="my-joke"> Awesome Joke is loading... </div>
 //       <button id="joke-Btn" class="btn">NEXT JOKE</button>
   const jokeUrl = ' https://icanhazdadjoke.com/'
@@ -6,7 +34,7 @@
           Accept : "application/json"
       }
   }
-
+ 
  const getJokes = () => {
      fetch(jokeUrl, configObj)
      .then((res) =>  res.json())
@@ -21,3 +49,5 @@
  const jokeBtn = document.getElementById('joke-Btn');
  jokeBtn.addEventListener('click', getJokes);
  getJokes();
+
+ 
